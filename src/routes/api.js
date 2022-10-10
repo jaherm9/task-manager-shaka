94cc3aeca451e3cb5e3a4662900c7a3f5501aa34
shaka <jaherm9@gmail.com>
2022-10-10 12:41:58 +0600

    task manager server

---
+++ src/routes/api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const UsersController = require("../controllers/UsersController");
+const AuthVerifyMiddleware = require("../middleware/AuthVerifyMiddleware");
+const TasksController = require("../controllers/TasksController");
+
+const router= express.Router();
+
+
+router.post("/registration", UsersController.registration)
+router.post("/login", UsersController.login)
+router.post("/profileUpdate",AuthVerifyMiddleware,UsersController.profileUpdate)
+
+
+router.post("/createTask",AuthVerifyMiddleware,TasksController.createTask)
+router.get("/updateTaskStatus/:id/:status",AuthVerifyMiddleware,TasksController.updateTaskStatus)
+router.get("/listTaskByStatus/:status",AuthVerifyMiddleware,TasksController.listTaskByStatus)
+router.get("/taskStatusCount",AuthVerifyMiddleware,TasksController.taskStatusCount)
+
+
+module.exports= router;
